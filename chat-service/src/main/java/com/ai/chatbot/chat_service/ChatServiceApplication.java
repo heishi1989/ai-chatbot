ac834fcfb587093f6bcd0334a15395b8c002f3c2
unknown <GTS@GTS-L-082.bstardev.info>
2025-11-12 00:29:12 +0700

    Init project

---
+++ chat-service/src/main/java/com/ai/chatbot/chat_service/ChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ai.chatbot.chat_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServiceApplication.class, args);
+	}
+
+}
